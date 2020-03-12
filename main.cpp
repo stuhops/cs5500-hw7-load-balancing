9f3ee865043c1f5708c3a4c39963e103f8a96429
stuhops <stuart.hopkins@aggiemail.usu.edu>
2020-03-12 16:36:54 -0600

    Looping with probe now works

--- main.cpp
+++ main.cpp
@@ -16,16 +16,26 @@ using namespace std::chrono;
 // TODO: Auto generate numbers and do auto send and receive
 
 void printTokenData(int rank, int size, int token, int special) {
-  cout << "----------------------------------" << endl
-       << "Rank    : " << rank << " / " << size-1 << endl
-       << "Token   : " << token << endl
-       << "Special : " << special << endl
-       << "----------------------------------"
+  cout << "Rank    : " << rank << " / " << size-1
+       << "      Token   : " << token
+       << "      Special : " << special
        << endl;
+  sleep(1);
+}
+void printTokenData(int rank, int size, int token, int special, int cnt) {
+  cout << "Rank    : " << rank << " / " << size-1 
+       << "    Token   : " << token 
+       << "    Special : " << special 
+       << "    Loop Cnt: " << cnt 
+       << endl;
+  sleep(1);
 }
 
 void print(string toPrint) {
   cout << toPrint << endl;
+}
+void print(string toPrint, int pInt) {
+  cout << toPrint << ": " << pInt << endl;
 }
 
 
@@ -37,59 +47,68 @@ int main(int argc, char **argv) {
   MPI_Comm_rank(MCW, &rank);
   MPI_Comm_size(MCW, &size);
   MPI_Status tokenStatus;
-  MPI_Status dataStatus;
+  MPI_Status workStatus;
+
+  const int TOKEN_TAG = 0;
+  const int WORK_COM = 1;
 
   int special = 0;
   bool workDone = true;
   int token = 2;  // 2 is white, 1 is black, 0 is terminate
   int tokenStarted = false;
+  
+  int token_flag;
+  int work_flag;
 
   if(rank == 2) special = 1;
+  if(rank == 1) special = 1;
 
   while(token) {
     // <<<<<<<<<<<<<<  Gather data from other processes >>>>>>>>>>>>>>>>>>>
-    // MPI_Probe((rank - 1 + size) % size, 0, MCW, &tokenStatus);
-
+    token_flag = 0;
+    MPI_Iprobe((rank - 1 + size) % size, TOKEN_TAG, MCW, &token_flag, &tokenStatus);
+    if(token_flag) {
+      MPI_Recv(&token, 1, MPI_INT, (rank - 1 + size) % size, TOKEN_TAG, MCW,MPI_STATUS_IGNORE);
+    }
 
     // <<<<<<<<<<<<<<<  Dual-pass ring termination >>>>>>>>>>>>>>>>>>>>>>>>
     if(workDone) {
       if(!rank){
         if(tokenStarted) {
-          // TODO: Receive a message from the last process and OR token with it
-          if(token == 2) {
-            token = 0;
+          if(token_flag) {
             printTokenData(rank, size, token, special);
-            MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, 0, MCW);
-          }
-          else {
-            token = 2;
-            printTokenData(rank, size, token, special);
-            MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, 0, MCW);
-            MPI_Recv(&token, 1, MPI_INT, (rank - 1 + size) % size, 0, MCW,MPI_STATUS_IGNORE);
+            if(token == 2) {
+              token = 0;
+              MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, TOKEN_TAG, MCW);
+            }
+            else {
+              token = 2;
+              MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, TOKEN_TAG, MCW);
+            }
           }
         }
         else {
           tokenStarted = true;
           token = 2;
           printTokenData(rank, size, token, special);
-          MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, 0, MCW);
-          MPI_Recv(&token, 1, MPI_INT, (rank - 1 + size) % size, 0, MCW,MPI_STATUS_IGNORE);
+          MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, TOKEN_TAG, MCW);
         }
       }
-      else {
-        // TODO: Receive a message from the last process and OR token with it
-        MPI_Recv(&token, 1, MPI_INT, (rank - 1 + size) % size, 0, MCW,MPI_STATUS_IGNORE);
+      else if(token_flag) {
         printTokenData(rank, size, token, special);
+
         if(token == 0) {
-          MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, 0, MCW);
+          MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, TOKEN_TAG, MCW);
           break;
         }
         else if(token == 2) 
           token = token - special;
 
         special = 0;
-        MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, 0, MCW);
+        MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, TOKEN_TAG, MCW);
       }
+
+      token_flag = 0;
     }
 
     else {
